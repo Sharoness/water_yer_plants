47eb6a35cf297ce10fa25889818d37dc275504af
Sharoness <sharong1234@hotmail.com>
2021-07-16 12:26:45 +0100

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import './App.css';
 import React, { useState } from 'react';
-import Start from './Start';
-import AddPlant from './AddPlant';
-import List from './List';
-import Info from './Info';
+
+import Start from './Start.jsx';
+import List from './List.jsx';
+import Layout from './components/Layout';
 
 function App() {
   const [plantlist, setPlantlist] = useState([]);
 
   return (
-    <div className="content">
-      <header>
-        <h1>Water yer plants</h1>
-      </header>
-      <Start />
-      {/* if state is empty -> component Start. Else component List */}
-    </div>
+      <Layout content={plantlist.length === 0 ? Start : List} />
   );
 }
 
